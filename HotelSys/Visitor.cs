6c1ca642f5d1f1d38b7570e314fe3388f793369a
SausageOG <sauageog@gmail.com>
2021-03-19 18:50:14 +0200

    Simple view and visitor model

---
+++ HotelSys/Visitor.cs
@@ -0,0 +1,25 @@
+﻿namespace HotelSys
+{
+    class Visitor
+    {
+        private string _name;
+        public string Name { get { return _name; } set { _name = value; } }
+
+        private string _female;
+        public string Female { get { return _female; } set { _female = value; } }
+
+        private int _age;
+        public int Age { get { return _age; } set { _age = value; } }
+
+        private int _room;
+        public int Room { get { return _room; } set { _room = value; } }
+        public Visitor() { }
+        public Visitor(string name, string female, int age, int room)
+        {
+            _name = name;
+            _female = female;
+            _age = age;
+            _room = room;
+        }
+    }
+}
