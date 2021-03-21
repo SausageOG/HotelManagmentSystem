314dd2366e8bcbe0d7b8491c2e21e37f95675941
SausageOG <sauageog@gmail.com>
2021-03-21 18:23:51 +0200

    Still work at view and create room model

---
+++ HotelSys/Models/Room.cs
@@ -0,0 +1,28 @@
+﻿namespace HotelSys.Models
+{
+    class Room
+    {
+        private int _id;
+        private string _title;
+        private int maxVisitors;
+
+
+        public Visitor[] visitors;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+            }
+        }
+
+
+        public Room(string title)
+        {
+
+        }
+
+    }
+}
---
+++ HotelSys/Models/Visitor.cs
@@ -0,0 +1,63 @@
+﻿namespace HotelSys
+{
+    class Visitor
+    {
+        private int _id;
+        private string _name;
+        private string _lastName;
+        private int _age;
+        private int _room;
+
+
+
+        public Visitor() { }
+        public Visitor(string name, string lastName, int age, int room)
+        {
+            _name = name;
+            _lastName = lastName;
+            _age = age;
+            _room = room;
+        }
+
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+            }
+        }
+        public int Room
+        {
+            get { return _room; }
+            set
+            {
+                _room = value;
+            }
+        }
+    }
+}
