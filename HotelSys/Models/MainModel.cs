11794591c16d71aa7a9f08488c7ff540d9902f14
SausageOG <sauageog@gmail.com>
2021-03-22 18:55:41 +0200

    A lot of details i did, read down below:
    1.Finished Room model(maybe)
    2.Finished Visitor model(maybe)
    3.Finished "Hotel visitors" tab
    4.Create "MainModel" and "MainViewModel" for connect models and view models
    5.Distributed models and view models
    6.Connected "MainViewModel" to window

---
+++ HotelSys/Models/MainModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace HotelSys.Models
+{
+    class MainModel
+    {
+        public Room room;
+
+
+        private ObservableCollection<Room> _rooms;
+
+
+        public ObservableCollection<Visitor> Visitors => room.Visitors;
+        public ObservableCollection<Room> Rooms { get { return _rooms; } set { _rooms = value; } }
+
+        public MainModel()
+        {
+            room = new Room();
+        }
+    }
+}
--- HotelSys/Models/Room.cs
+++ HotelSys/Models/Room.cs
@@ -1,14 +1,41 @@
-﻿namespace HotelSys.Models
+﻿using System.Collections.ObjectModel;
+
+namespace HotelSys.Models
 {
+    public enum RoomType
+    {
+        StandardRoom,
+        BusinessClassRoom,
+        JuniorSuite,
+        PresidentialSuite
+    }
     class Room
     {
+        private RoomType _roomType;
         private int _id;
         private string _title;
-        private int maxVisitors;
+        private int _max;
 
 
-        public Visitor[] visitors;
+        private ObservableCollection<Visitor> _visitors;
+        public ObservableCollection<Visitor> Visitors { get { return _visitors; } set { _visitors = value; } }
 
+        public RoomType Type
+        {
+            get { return _roomType; }
+            set
+            {
+                _roomType = value;
+            }
+        }
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                _id = value;
+            }
+        }
         public string Title
         {
             get { return _title; }
@@ -19,10 +46,19 @@
         }
 
 
-        public Room(string title)
+        public Room(string title, int max)
         {
-
+            _title = title;
+            _max = max;
         }
 
+        public void AddVisitor(Visitor visitor)
+        {
+            _visitors.Add(visitor);
+        }
+        public void DeleteVisitor(Visitor visitor)
+        {
+            _visitors.Remove(visitor);
+        }
     }
 }
